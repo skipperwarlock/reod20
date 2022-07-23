c518ee75b99cc43ece93145aa1705371fcce5f19
Sam <skipperwarlock@gmail.com>
2022-07-22 23:05:27 -0400

    update to mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useState} from 'react';
 import './App.css';
-import Button from '@material-ui/core/Button';
+import Button from '@mui/material/Button';
 import SUPPORT_IMAGES from './images.js'
 
 function App() {
